9d9ce692fe61b9224892c078801f7c58a114213f
Jonathan Dupré <419946+kareniel@users.noreply.github.com>
2024-05-25 17:55:06 -0400

    wip

--- src/components/circuit_builder.ts
+++ src/components/circuit_builder.ts
@@ -1,37 +1,47 @@
 import { registerComponent } from "../register";
 
 const block_types = ['mem_cell', 'xor', 'input'] as const
+type BlockType = typeof block_types[number]
 
 interface Block {
   id: number,
   value: number | '-',
-  type: typeof block_types,
-  out: undefined | number
+  type: BlockType,
+  out: number[]
+  in: number[]
 }
+
+registerComponent('dropdown', (o) => ({
+  open: o,
+  close() {
+    this.open = false
+  },
+  click() {
+    this.open = !this.open
+  }
+}))
 
 registerComponent('circuit_builder', () => ({
   init() {
-    (new Array(5)).fill(0).forEach(() => this.add())
-    this.blocks[4].value = 1
-    this.blocks[0].out = "2"
-    this.blocks[1].out = "3"
-    this.blocks[2].out = "4"
-    this.blocks[3].out = "5"
-    this.blocks[4].out = "1"
+    (new Array(5)).fill(0).forEach((_, index) => {
+      this.block_type = 'mem_cell'
+      const block = this.add('mem_cell')
+      block.out = index + 2 > 5 ? [1] : [index + 2]
+      block.value = 1
+    })
+    this.block_type = 'mem_cell'
   },
   id_counter: 1,
   tick: 0,
   blocks: [],
   block_types,
-  block_type: 'mem_cell',
+  block_type: '',
   mem_cells: [],
   update() {
     // disconnect blocks whose outputs don't exists anymore
     const ids = this.blocks.map(b => b.id)
     this.blocks.forEach(block => {
-      if (!ids.includes(Number(block.out))) {
-        block.out = 0
-      }
+      block.out = block.out.filter(out => ids.includes(Number(out)))
     })
 
     // update the mem cell list
@@ -40,13 +50,17 @@ registerComponent('circuit_builder', () => ({
   },
   add() {
     const block = {
+      id: this.id_counter++,
       type: this.block_type,
-      value: this.block_type === 'mem_cell' ? 0 : '-',
-      id: this.id_counter++
+      in: [],
+      value: this.block_type ? 0 : '-',
+      out: []
     }
 
     this.blocks.push(block)
+    this.block_type = ''
     this.update()
+    return block
   },
   remove(id) {
     const blocks = this.blocks as Block[]
@@ -62,20 +76,45 @@ registerComponent('circuit_builder', () => ({
       [curr.id]: curr.value
     }), {})
 
-    const tapped: number[] = []
+    const tapped = new Set<number>
 
-    blocks.forEach((inputBlock) => {
-      const outputBlock = blocks.find(b => b.id === Number(inputBlock.out))
+    // shift mem cell values
+    const cells = blocks.filter(block => block.type === 'mem_cell')
+    cells.forEach((inputBlock) => {
+      const outputBlocks = blocks.filter(b => inputBlock.out.includes(b.id))
+      if (outputBlocks) {
+        outputBlocks.forEach(outputBlock => {
+          console.log('Moving', inputBlock.id, 'into', outputBlock.id)
+          if (outputBlock.type === 'xor') {
+            outputBlock.in.push(previousValues[inputBlock.id])
+          } else {
+            outputBlock.value = previousValues[inputBlock.id]
+            tapped.add(outputBlock.id)
+          }
+        })
+      }
+    })
+
+    // apply gates
+    const gates = blocks.filter(block => block.type === 'xor')
+    gates.forEach(gate => {
+      const outputBlock = blocks.find(b => b.id === Number(gate.out))
       if (outputBlock) {
-        outputBlock.value = previousValues[inputBlock.id]
-        tapped.push(outputBlock.id)
+        if (gate.type === 'xor') {
+          const result = gate.in.reduce((prev, curr, i) => i === 0 ? curr : prev ^ curr, 0)
+          tapped.add(outputBlock.id)
+          outputBlock.value = result
+        }
       }
     })
 
     // empty the untapped blocks that have an output
     blocks
-      .filter(({id, out}) => !tapped.includes(id) && blocks.find(b => b.id === Number(out)))
-      .forEach(b => { b.value = 0; })
+      .filter(({id, out}) => !tapped.has(id) && blocks.some(b => b.id === Number(out)))
+      .forEach(b => {
+        console.log(b) 
+        b.value = 0;
+      })
 
     this.update()
   },
