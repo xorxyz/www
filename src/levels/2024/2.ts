f44c99ae33a510b6cead1107f03d4d1d2d4f671b
Jonathan Dupré <419946+kareniel@users.noreply.github.com>
2024-06-08 08:28:52 -0400

    use a build of amw for lvl 2

--- src/levels/2024/2.ts
+++ src/levels/2024/2.ts
@@ -1,3 +1,6 @@
 import { registerLevel } from '../../register'
 
-registerLevel('2024/2', () => ({}))
+// npx esbuild --bundle src/components/amw.ts --outfile=src/components/amw/builds/2024-06-08.js --format=cjs
+import amw from '../../components/amw/builds/2024-06-08.js'
+
+registerLevel('2024/2', amw)
